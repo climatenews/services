ebaa95869714edb7f7ad06477ffb16d697cd2a7c
Patrick Fitzgerald <patrickf@climateactioncollective.net>
2022-10-21 21:49:27 -0700

    UI redesigns

--- web/components/feature/about_content.tsx
+++ web/components/feature/about_content.tsx
@@ -1,31 +1,22 @@
+import NewsHeader from "components/generic/news_header";
+
 export default function AboutContent() {
   return (
-    <div>
-      {/* Content */}
-      <div className="relative py-8 sm:py-12 lg:py-16">
-        <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-          <img
-            className="mx-auto h-24 w-auto mt-10"
-            src="/cac_logo.svg"
-            alt="Climate Action Collective logo"
-          />
-
-          {/* <h1 className="text-base font-semibold uppercase tracking-wider text-primary">
-            Climate Action Collective
-          </h1> */}
-          <p className="mt-4 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            About Us
-          </p>
-          <p className="mx-auto mt-5 max-w-prose text-base text-gray-500">
-            Climate Action Collective is a benefit company registered in BC,
-            Canada in 2022.
-          </p>
-          <p className="mx-auto mt-1 max-w-prose text-base text-gray-500">
-            We aim to improve climate related education and make
-            tools for individuals and corporations to take meaningful climate
-            action.
-          </p>
-          {/* <p className="mt-8 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+    <>
+      <NewsHeader title="About" subtitle="Who we are" />
+      <div className="container px-4 w-full md:max-w-3xl mx-auto">
+        {/* Content */}
+        <div className="relative py-4">
+          <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+            <p className="mx-auto mt-5 max-w-prose">
+              Climate Action Collective is a benefit company registered in BC,
+              Canada in 2022.
+            </p>
+            <p className="mx-auto mt-1 max-w-prose">
+              We aim to improve climate related education and make tools for
+              individuals and corporations to take meaningful climate action.
+            </p>
+            {/* <p className="mt-8 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             Engaging climate content
           </p>
           <p className="mx-auto mt-5 max-w-prose text-xl text-gray-500">
@@ -34,8 +25,9 @@ export default function AboutContent() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut.
           </p> */}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
---
+++ web/components/generic/news_header.tsx
@@ -0,0 +1,23 @@
+
+interface NewsHeaderProps {
+    title: string;
+    subtitle: string;
+}
+
+export default function NewsHeader(props: NewsHeaderProps) {
+    return (
+        <>
+            <div className="py-2 lg:py-6 bg-gray-200">
+                <div className="container px-4 w-full md:max-w-3xl mx-auto ">
+                    <h1 className="font-bold font-sans break-normal text-gray-900 text-2xl">
+                        {props.title}
+                    </h1>
+                    <p className="text-sm font-light text-gray-600 mt-1">
+                        {props.subtitle}
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+{/* Top trending articles shared by{" "}<a className="hover:underline">climate scientists and activists</a> */ }
