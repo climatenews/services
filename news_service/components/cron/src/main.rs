7a2372a5db76543cbf6c0527b1241299ae1c7dae
Patrick Fitzgerald <patrickf@climatenews.app>
2022-12-09 21:03:53 -0800

    ...

--- news_service/components/cron/src/main.rs
+++ news_service/components/cron/src/main.rs
@@ -1,17 +1,8 @@
-use crate::news_feed::cron_job::cron_job;
 use actix_web::{get, web, App, HttpResponse, HttpServer, Result};
-use chrono::Local;
 use db::init_env;
-use db::models::news_cron_job::NewsCronJob;
-use db::sql::news_cron_job::insert_news_cron_job;
-use db::sql::news_cron_job::update_news_cron_job_completed_at;
-use db::sql::news_cron_job::update_news_cron_job_error;
-use db::util::convert::datetime_to_str;
-use db::util::convert::now_utc_datetime;
 use db::util::db::init_db;
-use log::error;
-use log::info;
-use sqlx::PgPool;
+use scheduler::main_scheduler::start_main_scheduler;
+use scheduler::tweet_scheduler::start_tweet_scheduler;
 use sqlx::Pool;
 use sqlx::Postgres;
 use std::env;
@@ -19,6 +10,7 @@ use std::env;
 pub mod language;
 pub mod news_feed;
 pub mod openai;
+pub mod scheduler;
 pub mod twitter;
 pub mod util;
 
@@ -47,9 +39,14 @@ pub async fn health(data: web::Data<AppState>) -> Result<HttpResponse> {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     init_env();
-    // Start scheduler on a new thread
+    // Start mainscheduler on a new thread
     actix_rt::spawn(async move {
-        start_scheduler().await;
+        start_main_scheduler().await;
+    });
+
+    // Start tweet scheduler on a new thread
+    actix_rt::spawn(async move {
+        start_tweet_scheduler().await;
     });
 
     let host = env::var("CRON_HOST").expect("HOST is not set");
@@ -64,45 +61,4 @@ async fn main() -> std::io::Result<()> {
         .unwrap_or_else(|_| panic!("Couldn't bind to port {}", port))
         .run()
         .await
-}
-
-pub async fn start_scheduler() {
-    info!("start_scheduler - {:?}", Local::now());
-    let db_pool = init_db().await;
-    loop {
-        // cron job continuous loop
-        if let Err(err) = start_cron_job(&db_pool).await {
-            error!("start_cron_job failed: {:?}", err);
-        }
-    }
-}
-
-pub async fn start_cron_job(db_pool: &PgPool) -> anyhow::Result<()> {
-    let start_datetime = now_utc_datetime();
-    let news_cron_job = NewsCronJob {
-        started_at: start_datetime.unix_timestamp(),
-        started_at_str: datetime_to_str(start_datetime),
-        completed_at: None,
-        completed_at_str: None,
-        error: None,
-    };
-
-    let news_cron_job_db = insert_news_cron_job(db_pool, news_cron_job).await?;
-    match cron_job(&db_pool).await {
-        Ok(_) => {
-            let completed_datetime = now_utc_datetime();
-            update_news_cron_job_completed_at(
-                &db_pool,
-                news_cron_job_db.id,
-                completed_datetime.unix_timestamp(),
-                datetime_to_str(completed_datetime),
-            )
-            .await?;
-        }
-        Err(err) => {
-            update_news_cron_job_error(&db_pool, news_cron_job_db.id, err.to_string()).await?;
-            error!("cron_job failed: {:?}", err);
-        }
-    }
-    Ok(())
 }
