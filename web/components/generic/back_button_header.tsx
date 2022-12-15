d147d19c2acc7f36a48bf275b3496af65b2b6d7b
Patrick Fitzgerald <patrick.fitzgerald.ca@gmail.com>
2022-12-14 19:54:33 -0800

    formatting

--- web/components/generic/back_button_header.tsx
+++ web/components/generic/back_button_header.tsx
@@ -7,17 +7,16 @@ export default function BackButtonHeader() {
     <>
       <div className="py-2 lg:py-6 bg-gray-200">
         <div className="container px-4 w-full md:max-w-3xl mx-auto">
-        <Link
-          href={{
-            pathname: "/",
-          }}
-          className="hover:underline"
-        >
+          <Link
+            href={{
+              pathname: "/"
+            }}
+            className="hover:underline"
+          >
             <p className="text-base font-bold text-gray-700 mt-1">
               &larr; Back
             </p>
-        </Link>
-
+          </Link>
         </div>
       </div>
     </>
