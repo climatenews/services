2e6ee1792d49ee3a59e1f24ef0b2822a59ac26a7
Patrick Fitzgerald <patrickf@climatenews.app>
2022-12-14 21:27:03 -0800

    Adding Posthog Analytics

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/generic/layout";
+import { usePostHog } from "next-use-posthog";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // Posthog Analytics
+  usePostHog("phc_ydNVAMi2gNQjqCrcCFTzlH9qzlfCHOD50UaanLHdXzf", {
+    api_host: "https://app.posthog.com",
+    loaded: (posthog) => {
+      // disable in development mode
+      if (process.env.NODE_ENV === "development") posthog.opt_out_capturing();
+    },
+    persistence: 'memory', // Avoids the use of cookies
+  });
+
   return (
     <Layout>
       <Component {...pageProps} />
