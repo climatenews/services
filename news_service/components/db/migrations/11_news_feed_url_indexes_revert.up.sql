10cfb105a99fb695d13f16d08d683cdd365f4753
Patrick Fitzgerald <patrick.fitzgerald.ca@gmail.com>
2022-12-24 11:09:18 -0800

    fix for migration error"

---
+++ news_service/components/db/migrations/11_news_feed_url_indexes_revert.up.sql
@@ -0,0 +1,4 @@
+DROP INDEX news_feed_url_is_climate_related_index;
+DROP INDEX news_feed_url_created_at_index;
+DROP INDEX news_feed_url_url_score_index;
+DROP INDEX news_feed_url_first_referenced_by_index;
