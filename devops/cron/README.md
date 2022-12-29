05005fd8b25e4c68d6787e8f33badd0ac59dd400
Patrick Fitzgerald <patrickf@climatenews.app>
2022-12-28 21:24:19 -0800

    updating ansible playbooks

---
+++ devops/cron/README.md
@@ -0,0 +1,8 @@
+
+### Postgresql backup script
+```bash
+# install script: 
+chmod +x postgres-backup.sh
+# run script: 
+sudo ./postgres-backup.sh
+```
--- devops/cron/postgres-backup.sh
+++ devops/cron/postgres-backup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+
 #configuration settings
-POSTGRES_USER=dbusername
-POSTGRES_DB=dbname
 CURRENT_MONTH=$(date +%Y-%m)
 CURRENT_DATE=$(date +%Y-%m-%d)
 CURRENT_DATETIME=$(date +%d-%b-%Y_%H_%M_%Z)
 BACKUPS_PATH=/backups
-DOCKER_SWARM_SERVICE_NAME=swarm_postgres
+DOCKER_SWARM_SERVICE_NAME=climate_news_stack_db
+
 ####################################
 #backup PostgreSQL database
 BACKUP_FOLDER=$BACKUPS_PATH/$CURRENT_MONTH/$CURRENT_DATE
@@ -14,18 +14,18 @@ if [ ! -d "$BACKUP_FOLDER" ]; then
     mkdir -p "$BACKUP_FOLDER"
 fi
 
-echo 'Creating PostgreSQL backups...'
+echo 'Creating PostgreSQL backup...'
 cd "$BACKUP_FOLDER"
 if [ -f 'dump_'"$POSTGRES_DB"'.sql' ]; then
    rm 'dump_'"$POSTGRES_DB"'.sql'
 fi
 db_backup_filename=$POSTGRES_DB'_'$CURRENT_DATETIME'.tar.gz'
 postgres_container_id=$(docker service ps -f "name=$DOCKER_SWARM_SERVICE_NAME" $DOCKER_SWARM_SERVICE_NAME -q --no-trunc | head -n1)
-docker exec -t symfony-blog_postgres.1."$postgres_container_id" pg_dump -U $POSTGRES_USER $POSTGRES_DB > 'dump_'"$POSTGRES_DB"'.sql'
+docker exec -t $DOCKER_SWARM_SERVICE_NAME.1."$postgres_container_id" pg_dump -U $POSTGRES_USER $POSTGRES_DB > 'dump_'"$POSTGRES_DB"'.sql'
 tar -cf - 'dump_'"$POSTGRES_DB"'.sql' | gzip -9 > "$db_backup_filename"
 rm 'dump_'"$POSTGRES_DB"'.sql'
 
 cd "$BACKUP_FOLDER"
 md5sum * > MD5SUMS
 
-echo 'Done.'
+echo 'Done. '
