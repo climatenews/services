6d70c1e2d1bfd3c0f514a9fbead09cb6b332c4bc
Patrick Fitzgerald <patrickf@climatenews.app>
2023-01-06 15:58:56 -0800

    REenabling tweet scheduler

--- news_service/components/cron/src/news_feed/constants.rs
+++ news_service/components/cron/src/news_feed/constants.rs
@@ -7,14 +7,14 @@ pub fn twitter_lists() -> Vec<i64> {
     if cfg!(debug_assertions) {
         // 1 list in debug mode
         vec![
-            1586920047964205057, // Climate News - @climatenews_app - https://twitter.com/i/lists/1586920047964205057
+            1586920047964205057, // Climate News - @climatenews_app - https://twitter.com/i/lists/1586920047964205057 - 59 Members
         ]
     } else {
         // 3 lists in release mode
         vec![
-            1586920047964205057, // Climate News - @climatenews_app - https://twitter.com/i/lists/1586920047964205057
-            1053067173961326594, // scientists who do climate - @KHayhoe - https://twitter.com/i/lists/1053067173961326594
-            1308140854524162059, // Tweets about climate change from journalists, policy specialists, and institutions. https://twitter.com/i/lists/1308140854524162059
+            1586920047964205057, // Climate News - @climatenews_app - https://twitter.com/i/lists/1586920047964205057 - 59 Members
+            1053067173961326594, // scientists who do climate - @KHayhoe - https://twitter.com/i/lists/1053067173961326594 - 3.1K Members
+            1308140854524162059, // Tweets about climate change from journalists, policy specialists, and institutions. https://twitter.com/i/lists/1308140854524162059 - 231 Members
         ]
     }
 }
