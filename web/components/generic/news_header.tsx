2fbaafc7ec7fe0ed02fdafd3a5b838b00e781e26
Patrick Fitzgerald <patrick.fitzgerald.ca@gmail.com>
2023-01-06 20:40:53 -0800

    UI fixes

--- web/components/generic/news_header.tsx
+++ web/components/generic/news_header.tsx
@@ -7,7 +7,7 @@ interface NewsHeaderProps {
 export default function NewsHeader(props: NewsHeaderProps) {
   return (
     <>
-      <div className="py-2 bg-gray-200">
+      <div className="py-4 bg-gray-200">
         <div className="container px-4 w-full md:max-w-3xl mx-auto ">
           {props.title ? (
             <h1 className="font-bold font-sans break-normal text-gray-900 text-2xl sm:text-xl">
