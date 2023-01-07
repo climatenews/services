b6fa84f6865cd48c972cb4a8c7cf6340098945f9
Patrick Fitzgerald <patrick.fitzgerald.ca@gmail.com>
2023-01-07 14:13:48 -0800

    Updating favicons

--- web/components/generic/meta.tsx
+++ web/components/generic/meta.tsx
@@ -44,7 +44,17 @@ const Meta: NextPage<Props> = ({
       <meta name="keywords" content={keywords}></meta>
       <link rel="canonical" href={websiteUrl} />
       <meta name="description" content={description} />
-      <link rel="icon" href="/favicon.ico" />
+
+      {/* Favicon */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="apple-mobile-web-app-title" content="Climate News" />
+      <meta name="application-name" content="Climate News" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="theme-color" content="#ffffff" />
+
       <title>{title}</title>
       {/* Open Graph */}
       <meta property="og:url" content={websiteUrl} />
